cc73ecc23ea5f539bc1c558c626d8cec498da56c
Eryk <erykkurkowski.praca@gmail.com>
2023-05-30 12:24:59 +0200

    Support for the ModelForm class in the view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 class PostListView(ListView):
@@ -34,7 +34,27 @@ def post_detail(request, year, month, day, post):
 								   publish__year=year,
 								   publish__month=month,
 								   publish__day=day)
-	return render(request, 'blog/post/detail.html', {'post' : post})
+
+	#Lista aktywnych komentarzy dla danego posta.
+	comment = post.comments.filter(active=True)
+
+	if request.method == 'POST':
+		#Komentarz został opublikowany
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			#Utworzenie obiektu Comment; jeszcze jednak nie zapisujemy go w
+			#bazie danych.
+			new_comment = comment_form.save(commit=False)
+			#Przypisanie komentarza do bieżącego posta.
+			new_comment.post = post
+			#Zapisanie komentarza w bazie danych.
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+	return render(request, 'blog/post/detail.html', 
+							{'post' : post,
+							 'comments': comments,
+							 'comment_form': comment_form})
 
 def post_share(request, post_id):
 	#Pobieranie posta na podstawie jego identyfikatora.
