f7fb03b8ca75c1f5ebf2198fb10a92d2fec74279
Eryk <erykkurkowski.praca@gmail.com>
2023-05-31 19:09:19 +0200

    Adding tag functionality

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,32 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
+from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
-from django.core.mail import send_mail
 
-class PostListView(ListView):
-	queryset = Post.published.all()
-	context_object_name = 'posts'
-	paginate_by = 3
-	template_name = 'blog/post/list.html'
 
-# def post_list(request):
-# 	object_list = Post.published.all()
-# 	paginator = Paginator(object_list, 3) #Trzy posty na każdej stronie
-# 	page = request.GET.get('page')
-# 	try:
-# 		posts = paginator.page(page)
-# 	except PageNotAnInteger:
-# 		#Jeżeli zmienna page nie jest liczbą całkowitą,
-# 		#wówczas pobierana jest pierwsza strona wyników.
-# 		posts = paginator.page(1)
-# 	except EmptyPage:
-# 		#Jeżeli zmienna page ma wartośc większą niż numer ostatniej strony
-# 		#wyników, wtedy pobierana jest ostatnia strona wyników.
-# 		posts = paginator.page(paginator.num_pages)
-# 	return render(request, 'blog/post/list.html', 
-# 		{'page': page, 'posts': posts})
+# class PostListView(ListView):
+# 	queryset = Post.published.all()
+# 	context_object_name = 'posts'
+# 	paginate_by = 3
+# 	template_name = 'blog/post/list.html'
+
+def post_list(request, tag_slug=None):
+	object_list = Post.published.all()
+	tag = None
+
+	if tag_slug:
+		tag = get_object_or_404(Tag, slug=tag_slug)
+		object_list = object_list.filter(tags__in=[tag])
+
+	paginator = Paginator(object_list, 3) #Trzy posty na każdej stronie
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		#Jeżeli zmienna page nie jest liczbą całkowitą,
+		#wówczas pobierana jest pierwsza strona wyników.
+		posts = paginator.page(1)
+	except EmptyPage:
+		#Jeżeli zmienna page ma wartośc większą niż numer ostatniej strony
+		#wyników, wtedy pobierana jest ostatnia strona wyników.
+		posts = paginator.page(paginator.num_pages)
+	return render(request, 'blog/post/list.html', 
+		{'page': page, 'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
