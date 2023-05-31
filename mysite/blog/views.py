ae48b803784a50acd2c848b003ea740cd2687289
Eryk <erykkurkowski.praca@gmail.com>
2023-05-31 19:45:08 +0200

    Implementation of downloading similar posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
@@ -33,6 +34,7 @@ def post_list(request, tag_slug=None):
 		#Jeżeli zmienna page ma wartośc większą niż numer ostatniej strony
 		#wyników, wtedy pobierana jest ostatnia strona wyników.
 		posts = paginator.page(paginator.num_pages)
+
 	return render(request, 'blog/post/list.html', 
 		{'page': page, 'posts': posts, 'tag': tag})
 
@@ -59,10 +61,19 @@ def post_detail(request, year, month, day, post):
 			new_comment.save()
 	else:
 		comment_form = CommentForm()
+
+
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+								 .exclude(id = post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+							   .order_by('-same_tags', '-publish')[:4]
+
 	return render(request, 'blog/post/detail.html', 
 							{'post' : post,
 							 'comments': comments,
-							 'comment_form': comment_form})
+							 'comment_form': comment_form,
+							 'similar_posts': similar_posts})
 
 def post_share(request, post_id):
 	#Pobieranie posta na podstawie jego identyfikatora.
